926a0a7783056d73e51385973be6c21a9c554b65
catalinbadila <112496548+cbd23@users.noreply.github.com>
2025-01-16 12:03:00 +0200

    Create the Vue app.

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Lamp',
+            description: 'This is one of the most amazing lamps in the world.'
+        }
+    }
+})
